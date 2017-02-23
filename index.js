366111380c8e7eace412eaba0d628d87dfa622e5
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2017-02-23 15:38:48 -0300

    Simple hello world

---
+++ index.js
@@ -0,0 +1,22 @@
+var express    = require('express');
+var app        = express();
+var client 	   = require('redis').createClient(process.env.REDIS_URL);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'Hello World' });   
+});
+
+
+app.use('/', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
