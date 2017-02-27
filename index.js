70a93c05a96fba073e85ac9eca815f301bf753ed
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2017-02-27 16:06:47 -0300

    Fix wrong object usage

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 var router = express.Router();
 
 router.get('/:animal', function(req, res) {
-	res.json({ 'animal': req.route.params.animal });
+	res.json({ 'animal': req.params.animal });
 	// client.get('iOS', function(err, reply) {
 	    // res.json({ 'iOS': reply || 0 });   
 	// });
