7b8e233e598d3ac245c7d9632e97eb5faceab015
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2017-02-27 17:00:45 -0300

    Use sha.js instead of sha256

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express    = require('express');
 var app        = express();
 var client 	   = require('redis').createClient(process.env.REDIS_URL);
-var sha256	   = require('sha256');
+var createHash = require('sha.js');
+var sha256	   = createHash(sha256);
+
 
 var port = process.env.PORT || 8080;
 
@@ -37,7 +39,7 @@ router.delete('/:animal', (req, res) => {
 		res.json({ error: 'Please provide a password'});
 	}
 
-	var hashedPassword = sha256.sha256(password);
+	var hashedPassword = sha256.update(password, 'utf8').digest('hex');
 	if (hashedPassword == '171f0e636fc5a5ce5a3ada753320ee682e4876e752ac42dd891eda1ea6f4517f') {
 		client.delete(animal, (error, reply) => {
 			res.send(200)
