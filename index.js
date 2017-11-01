920a16c84793c1439ebda8dbae1e594e4363992b
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2017-10-31 23:21:32 -0700

    Fix formatting and use ES6

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-var express    = require('express');
-var app        = express();
-var client 	   = require('redis').createClient(process.env.REDIS_URL);
-var createHash = require('sha.js');
+const express = require('express')
+const app = express()
+const client = require('redis').createClient(process.env.REDIS_URL)
+const createHash = require('sha.js')
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
-var router = express.Router();
+const router = express.Router()
 
 router.get('/', (req, res) => {
 	client.keys('*', (err, keys) => {
-		res.json({ animals: keys });
-	});
-});
+		res.json({ animals: keys })
+	})
+})
 
 router.get('/:animal', (req, res) => {
-	var animal = req.params.animal;
+	const animal = req.params.animal
 	client.get(animal, (err, reply) => {
-	    res.json({ [animal]: reply || 0 });
+		res.json({ [animal]: reply || 0 })
 	});
 });
 
 router.post('/:animal', (req, res) => {
-	var animal = req.params.animal;
-	client.incr(animal);
+	const animal = req.params.animal
+	client.incr(animal)
 	client.get(animal, (err, reply) => {
-	    res.json({ [animal]: reply || 0 });
-	});
-});
+		res.json({ [animal]: reply || 0 })
+	})
+})
 
 router.delete('/:animal', (req, res) => {
-	var animal = req.params.animal;
-	var password = req.query.password;
+	const animal = req.params.animal
+	const password = req.query.password
 
 	if (password == undefined) {
 		res.statusCode = 400
-		res.json({ error: 'Please provide a password'});
+		res.json({ error: 'Please provide a password' })
 	}
-	var sha256	   = createHash('sha256');
-	var hashedPassword = sha256.update(password, 'utf8').digest('hex');
+	var sha256 = createHash('sha256')
+	var hashedPassword = sha256.update(password, 'utf8').digest('hex')
 	if (hashedPassword == '171f0e636fc5a5ce5a3ada753320ee682e4876e752ac42dd891eda1ea6f4517f') {
 		client.del(animal, (error, reply) => {
 			res.send(200)
-		});
+		})
 	} else {
 		res.statusCode = 401
-		res.json({ error: 'Invalid password'});
+		res.json({ error: 'Invalid password' })
 	}
-});
+})
 
-app.use('/', router);
+app.use('/', router)
 
-app.listen(port);
-console.log('Listening to ' + port);
+app.listen(port)
+console.log('Listening to ' + port)
